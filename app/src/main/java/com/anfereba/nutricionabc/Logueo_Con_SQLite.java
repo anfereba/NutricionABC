ca3231d54b1af9e33807f6fbe852428b09fdb403
Fabian <fabian0647@hotmail.es>
2021-09-25 12:43:22 -0500

    Vista para iniciar sesion conn SQLite//fabian

---
+++ app/src/main/java/com/anfereba/nutricionabc/Logueo_Con_SQLite.java
@@ -0,0 +1,14 @@
+package com.anfereba.nutricionabc;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Logueo_Con_SQLite extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_logueo_con_sqlite);
+    }
+}
