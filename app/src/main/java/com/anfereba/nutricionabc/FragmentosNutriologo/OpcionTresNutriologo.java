677f26d2d736d9333f145bc809b9aa24792cb6d6
Fabian <fabian0647@hotmail.es>
2021-10-01 18:24:38 -0500

    implemente las operaciones dentro de los fragmentos

--- app/src/main/java/com/anfereba/nutricionabc/FragmentosNutriologo/OpcionTresNutriologo.java
+++ app/src/main/java/com/anfereba/nutricionabc/FragmentosNutriologo/OpcionTresNutriologo.java
@@ -8,6 +8,8 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.anfereba.nutricionabc.R;
 
@@ -17,6 +19,7 @@ import com.anfereba.nutricionabc.R;
  * create an instance of this fragment.
  */
 public class OpcionTresNutriologo extends Fragment {
+    Button button;
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -63,6 +66,14 @@ public class OpcionTresNutriologo extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_opcion_tres_nutriologo, container, false);
+        View v=inflater.inflate(R.layout.fragment_opcion_tres_nutriologo, container, false);
+        Button button= (Button) v.findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getContext(),"buena",Toast.LENGTH_LONG).show();
+            }
+        });
+        return v;
     }
 }
