12ff05897daceaafd27d408600a1bd360863c66f
Fabian <fabian0647@hotmail.es>
2021-11-01 17:37:26 -0500

    Los Padres Podran compartir un plan nutricional por redes sociales//toca que tengan instalado la aplicacion con la que quieren compartir

--- app/src/main/java/com/anfereba/nutricionabc/FragmentosNutriologo/PlanDirario.java
+++ app/src/main/java/com/anfereba/nutricionabc/FragmentosNutriologo/PlanDirario.java
@@ -4,8 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.text.InputType;
 import android.view.KeyEvent;
 import android.view.View;
@@ -27,7 +31,10 @@ import com.anfereba.nutricionabc.db.Entidades.PlanesDiarios;
 import com.anfereba.nutricionabc.db.Entidades.PlanesNutricionales;
 import com.anfereba.nutricionabc.db.utilidades.Utilidades;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.util.ArrayList;
+import java.util.Date;
 
 public class PlanDirario extends AppCompatActivity {
 int IdHijo =0;
@@ -36,7 +43,8 @@ TextView PlanDiario;
 RecyclerView AlimentosDiarios;
 EditText ComentarioNutriologo;
 Button GuardarComentarioDiario, VistoBueno;
-ImageView IMGAtras;
+ImageView IMGAtras,SharePlan;
+    Uri uri;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -47,10 +55,42 @@ ImageView IMGAtras;
         VistoBueno = findViewById(R.id.VistoBueno);
         VistoBueno.setVisibility(View.INVISIBLE);
         IMGAtras = findViewById(R.id.IMGAtras);
+        SharePlan = findViewById(R.id.SharePlan);
         IMGAtras.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 onBackPressed();
+            }
+        });
+        SharePlan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(takeScreenshot()!=null){
+                    Date now = new Date();
+                    android.text.format.DateFormat.format("yyyy-MM-dd_hh:mm:ss", now);
+                    try {
+                        // nombre y ruta de la imagen a incluir
+                        String mPath = Environment.getExternalStorageDirectory().toString() + "/" + now + ".jpg";
+                        File imageFile = new File(mPath);
+                        FileOutputStream outputStream = new FileOutputStream(imageFile);
+                        int quality = 100;
+                        Bitmap bitmap=takeScreenshot();
+                        bitmap.compress(Bitmap.CompressFormat.JPEG, quality, outputStream);
+                        outputStream.flush();
+                        outputStream.close();
+                        uri = Uri.fromFile(imageFile);
+
+                    } catch (Throwable e) {
+                        // Captura los distintos errores que puedan surgir
+                        e.printStackTrace();
+                    }
+                    Bitmap bitmap = takeScreenshot();
+                    Intent intent = new Intent(Intent.ACTION_SEND);
+                    intent.setType("image/*");
+                    intent.putExtra(Intent.EXTRA_STREAM, uri);
+                    startActivity(Intent.createChooser(intent, "Share with"));
+                }
+
             }
         });
         if (savedInstanceState == null) {
@@ -77,9 +117,6 @@ ImageView IMGAtras;
                 Toast.makeText(PlanDirario.this,"Comentario Guardado",Toast.LENGTH_LONG).show();
             }
         });
-
-
-
         /*DbHijo dbHijo = new DbHijo(this);
         AlimentosDiarios = (RecyclerView) findViewById(R.id.AlimentosDiarios);
         AlimentosDiarios.setLayoutManager(new LinearLayoutManager(this));
@@ -105,6 +142,8 @@ ImageView IMGAtras;
             GuardarComentarioDiario.setInputType(InputType.TYPE_NULL);
             VistoBueno.setVisibility(View.INVISIBLE);
             VistoBueno.setInputType(InputType.TYPE_NULL);
+        }else {
+            SharePlan.setVisibility(View.INVISIBLE);
         }
         if(dbPlanNutricional.verPorcentagePlan(IdHijo,IdPlanNutricional)==100&&u==2){
             VistoBueno.setVisibility(View.VISIBLE);
@@ -133,5 +172,19 @@ ImageView IMGAtras;
             //((MainActivityCliente)getApplicationContext()).finish();
         }
         return super.onKeyDown(keyCode, event);
+    }
+    private Bitmap takeScreenshot() {
+        Bitmap bitmap= null;
+        try {
+            // crear un bitmap con la captura de pantalla
+            View v1 = getWindow().getDecorView().getRootView();
+            v1.setDrawingCacheEnabled(true);
+            bitmap = Bitmap.createBitmap(v1.getDrawingCache());
+            v1.setDrawingCacheEnabled(false);
+        } catch (Throwable e) {
+            // Several error may come out with file handling or OOM
+            e.printStackTrace();
+        }
+        return bitmap;
     }
 }
